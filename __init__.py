eff9f19fc0b9c00992a3e703cb4b4c902dbcaa97
Andrei Korostelev <andrei.korostelev@sioux.eu>
2015-06-26 10:44:38 +0200

    Use relative import within a package
    So it also works in Python 3 under Windows

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pretty_python import check_shebang, check_coding, check_pep8
-from pretty_python import fix_shebang, fix_coding, fix_pep8
-from pretty_python import install_deps
+from .pretty_python import check_shebang, check_coding, check_pep8
+from .pretty_python import fix_shebang, fix_coding, fix_pep8
+from .pretty_python import install_deps
